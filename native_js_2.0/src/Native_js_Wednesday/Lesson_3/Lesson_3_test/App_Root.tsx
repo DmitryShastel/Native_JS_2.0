c0a9cf71ed6b44399a327aeb407de99e68b53b9a
unknown <dmshastel@gmail.com>
2023-12-12 14:56:23 +0300

    js_wednesday: Lesson 3 test getPosts async is created

--- native_js_2.0/src/Native_js_Wednesday/Lesson_3/Lesson_3_test/App_Root.tsx
+++ native_js_2.0/src/Native_js_Wednesday/Lesson_3/Lesson_3_test/App_Root.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {getPosts} from "./api";
 
 export const AppRoot = () => {
+
+    // useEffect(() => {
+    //     getPosts
+    //         .then(res => console.log(res))
+    //         .catch(err => console.log(err))
+    // })
+
+    let getPostDataHandler = () => {
+        return getPosts()
+    }
+
+
     return (
         <div>
-           <button onClick={() => {}}>get data</button>
+            <button onClick={getPostDataHandler}>get posts data
+            </button>
         </div>
     );
 };
--- native_js_2.0/src/Native_js_Wednesday/Lesson_3/Lesson_3_test/api.ts
+++ native_js_2.0/src/Native_js_Wednesday/Lesson_3/Lesson_3_test/api.ts
@@ -1,1 +1,16 @@
-export {}
+import axios from 'axios';
+
+
+const FIRST_API_URL = 'https://jsonplaceholder.typicode.com';
+const SECOND_API_URL = 'https://fakeapi.com'
+
+export const getPosts = async () => {
+    try {
+        axios({
+            url: `${FIRST_API_URL}/users/`,
+            method: 'get'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
