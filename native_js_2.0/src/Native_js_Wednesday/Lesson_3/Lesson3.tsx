3fd0a62f5b411619598f7c20505e805d5d8d9f6e
unknown <dmshastel@gmail.com>
2023-12-18 13:43:01 +0300

    js_wednesday: Lesson 3 jsonData is added

--- native_js_2.0/src/Native_js_Wednesday/Lesson_3/Lesson3.tsx
+++ native_js_2.0/src/Native_js_Wednesday/Lesson_3/Lesson3.tsx
@@ -4,64 +4,72 @@ import {API} from './API';
 export const Lesson3 = () => {
 
     const [searchName, setSearchName] = useState('');
-    const [serachResult, setSerachResult] = useState('');
+    const [searchResult, setSearchResult] = useState('');
     const [searchNameByType, setSearchNameByType] = useState('');
-    const [serachResultByType, setSerachResultByType] = useState('');
+    const [searchResultByType, setSearchResultByType] = useState('');
+    const [jsonData, setJsonData] = useState(null);
 
 
-    // const searchFilm = () => {
-    //     API.searchFilmsByTitle(searchName)
-    //         .then(res => {
-    //             console.log(res)
-    //             if (res.data.Response === 'True') {
-    //                 setSerachResult(JSON.stringify(res.data.Search))
-    //             } else {
-    //                 console.log(res.data)
-    //                 setSerachResult(res.data.Error)
-    //             }
-    //         })
-    // };
+    //Поиск фильма через кнопку
+    const searchFilm = () => {
+        API.searchFilmsByTitle(searchName)
+            .then(res => {
+                console.log(res)
+                if (res.data.Response === 'True') {
+                    setSearchResult(JSON.stringify(res.data.Search))
+                } else {
+                    console.log(res.data)
+                    setSearchResult(res.data.Error)
+                }
+            })
+    };
 
-    // const searchFilm = () => {
-    //     API.searchFilmsByTitle(searchName)
-    //         .then((res) => {
-    //             console.log(res);
-    //             if (res.data.Response === 'True') {
-    //                 setSerachResult(JSON.stringify(res.data.Search));
-    //             } else {
-    //                 console.log(res.data);
-    //                 setSerachResult(res.data.Error);
-    //             }
-    //         });
-    // };
-
+    //Поиск фильма через ввод названия фильма
     useEffect(() => {
         const delaySearch = setTimeout(() => {
             API.searchFilmsByTitle(searchName)
                 .then((res) => {
                     console.log(res);
                     if (res.data.Response === 'True') {
-                        setSerachResult(JSON.stringify(res.data.Search));
-                    } else {
-                        console.log(res.data);
-                        setSerachResult(res.data.Error);
+                        setSearchResult(JSON.stringify(res.data.Search));
+                    } else if (res.data.Error !== "Incorrect IMDb ID.") {
+                        console.log(res.data.Error);
+                        setSearchResult(res.data.Error);
                     }
                 });
         }, 500); // Задержка в 500 миллисекунд для ожидания ввода
-
         return () => clearTimeout(delaySearch); // Очистка таймера при каждом изменении значения в поле ввода
     }, [searchName]);
 
 
+    //Поиск фильма через ввод названия фильма
+    useEffect(() => {
+        const delaySearchType = setTimeout(() => {
+                API.searchFilmsByType(searchNameByType, '')
+                    .then((res) => {
+                        console.log(res);
+                        if (res.data.Response === 'True') {
+                            setSearchResultByType(JSON.stringify(res.data.Search));
+                        } else if(res.data.Error !== "Incorrect IMDb ID.") {
+                            setSearchResultByType(res.data.Error);
+                        }
+                    });
+
+        }, 500);
+
+        return () => clearTimeout(delaySearchType);
+    }, [searchNameByType]);
+
+    //Поиск типа фильма или сериала через кнопку
     const searchByType = (e: React.MouseEvent<HTMLButtonElement>) => {
         const type: string = e.currentTarget.dataset.t ? e.currentTarget.dataset.t : '';
         API.searchFilmsByType(searchNameByType, type)
             .then(res => {
                 console.log(res)
                 if (res.data.Response === 'True') {
-                    setSerachResultByType(JSON.stringify(res.data.Search))
+                    setSearchResultByType(JSON.stringify(res.data.Search))
                 } else {
-                    setSerachResultByType(res.data.Error)
+                    setSearchResultByType(res.data.Error)
                 }
             })
     }
@@ -71,32 +79,34 @@ export const Lesson3 = () => {
         setSearchName(value);
     }
 
-    // const handleSearchButtonClick = () => {
-    //     searchFilm();
-    // }
-
+    const handleSearchTypeInputChange = (event: any) => {
+        const {value} = event.target;
+        setSearchNameByType(value);
+    };
 
     return (
         <div>
             <h1>Promises</h1>
             <div>
                 <h3><p>Search by name:</p></h3>
-               {/*<input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>*/}
+                {/*<input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>*/}
                 <input type="text" value={searchName} onChange={handleSearchInputChange}/>
-                <button onClick={() => {}}>Search</button>
+                <button onClick={searchFilm}>Search</button>
                 <div>
-                    {serachResult}
+                    {searchResult}
                 </div>
             </div>
 
             <div>
                 <h3><p>Search by type:</p></h3>
+                {/*<input type="text" value={searchNameByType}*/}
+                {/*       onChange={(e) => setSearchNameByType(e.currentTarget.value)}/>*/}
                 <input type="text" value={searchNameByType}
-                       onChange={(e) => setSearchNameByType(e.currentTarget.value)}/>
+                       onChange={handleSearchTypeInputChange}/>
                 <button onClick={searchByType} data-t='movie'>Movie</button>
                 <button onClick={searchByType} data-t='series'>Series</button>
                 <div>
-                    {serachResultByType}
+                    {searchResultByType}
                 </div>
             </div>
         </div>
