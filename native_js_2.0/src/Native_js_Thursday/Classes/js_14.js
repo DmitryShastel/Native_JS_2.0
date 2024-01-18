e173293cc38a476a0099520fb78aef8394d050a0
unknown <dmshastel@gmail.com>
2024-01-18 09:14:47 +0300

    js_thursday: Lesson 2 some expretions with this are done

--- native_js_2.0/src/Native_js_Thursday/Classes/js_14.js
+++ native_js_2.0/src/Native_js_Thursday/Classes/js_14.js
@@ -13,7 +13,7 @@
 // const u1 = new User('Dima', 'iy-incubator', new Date(1990, 11, 8))
 // console.log(u1)
 
-'use strict';
+// 'use strict';
 
 
 // const user = {
@@ -25,14 +25,82 @@
 //
 // user.showName()
 
-const user = {
-    age: 23,
-    showAge() {
-        const foo = () => {
-            console.log(this.age)
-        }
-        foo.call({age: 12})
-    }
-}
+// const user = {
+//     age: 23,
+//     showAge() {
+//         const foo = () => {
+//             console.log(this.age)
+//         }
+//         foo.call({age: 12})
+//     }
+// }
+//
+// user.showAge.call({age: 20})
 
-user.showAge({age: 20})
+// const a = {
+//     msg: 'Hello World',
+//
+//     getMsg() {
+//         const msg = 'Hello'
+//         return this.msg
+//     },
+//
+//     getMsg2: () => {
+//         const msg = 'Hello'
+//         return this.msg
+//     }
+// }
+//
+// console.log(a.getMsg())
+// console.log(a.getMsg2())
+
+
+// this.name = 'GLOBAL'
+
+// const a = {
+//     name: 'a',
+//
+//     go: function () {
+//         console.log(this.name)
+//         //a
+//     },
+//
+//     run: () => {
+//         console.log(this.name)
+//         //'GLOBAL'
+//     },
+//
+//     stop: function () {
+//         (() => {
+//             console.log(this.name)
+//         }).call({name: 'call'})
+//     }
+// }
+//
+// a.go()
+// a.run()
+// a.stop()
+
+
+// const test = {
+//     prop: 42,
+//     func: function () {
+//         return this.prop
+//     }
+// }
+//
+//
+// function f1() {
+//     return this.a
+// }
+//
+// let g = f1.bind(({a: 'hello'}))
+// let h = g.bind(({a: 'hello2'}))
+//
+// console.log(g())
+// console.log(h())
+
+
+
+
+
